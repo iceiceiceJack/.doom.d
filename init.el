edd15e4b492a398dc2967e4a084ea75828a8ef78
iceiceiceJack <zhangk1991@gmail.com>
2023-05-06 16:02:24 +0800

    add vterm

--- init.el
+++ init.el
@@ -25,7 +25,8 @@
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       vertico           ; the search engine of the future
+       (vertico           ; the search engine of the future
+        +icons)
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -77,7 +78,7 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
