f1225894a8ffeab2bfb1f0e3251f36d2df42c374
iceiceiceJack <zhangk1991@gmail.com>
2023-05-06 19:54:27 +0800

    add init

--- +misc.el
+++ +misc.el
@@ -2,15 +2,13 @@
 
 
 (setq user-full-name "Jackie Zhang"
-      user-mail-address "i@codumn.com"
-
-      )
+      user-mail-address "i@codumn.com")
 
 (setq package-archives '(("gnu"             . "https://mirrors.cloud.tencent.com/elpa/gnu/")
                          ("melpa"           . "https://mirrors.cloud.tencent.com/elpa/melpa/")
                          ("melpa-stable"    . "https://mirrors.cloud.tencent.com/elpa/melpa-stable/")
-                         ("org"             . "https://mirrors.cloud.tencent.com/elpa/org/"))
-      )
+                         ("org"             . "https://mirrors.cloud.tencent.com/elpa/org/")))
+
 (when (and IS-MAC window-system)
   (setq doom-theme 'doom-solarized-light
         ns-use-thin-smoothing t)
@@ -18,8 +16,13 @@
   (add-hook 'window-setup-hook #'toggle-frame-maximized)
   ;; (add-hook 'window-setup-hook #'toggle-frame-fullscreen)
   )
+
 ;; (unless window-system
 ;;   (setq doom-theme 'doom-nord)
 ;;   (global-set-key [mouse-4] 'scroll-down-line)
 ;;   (global-set-key [mouse-5] 'scroll-up-line)
 ;;   )
+
+
+;;; :ui doom-dashboard
+;; (setq fancy-splash-image (concat doom-user-dir "splash.png"))
