41a9c036655af13d0a136f612130c7994afda849
iceiceiceJack <zhangk1991@gmail.com>
2023-05-18 11:49:26 +0800

    add package sis

--- +misc.el
+++ +misc.el
@@ -26,3 +26,26 @@
 
 ;;; :ui doom-dashboard
 ;; (setq fancy-splash-image (concat doom-user-dir "splash.png"))
+
+
+(use-package sis
+  ;; :hook
+  ;; ;; enable the /context/ and /inline region/ mode for specific buffers
+  ;; (((text-mode prog-mode) . sis-context-mode)
+  ;;  ((text-mode prog-mode) . sis-inline-mode))
+  :config
+  ;; For MacOS
+  (sis-ism-lazyman-config
+   ;; English input source may be: "ABC", "US" or another one.
+   "com.apple.keylayout.ABC"
+   ;; Other language input source: "rime", "sogou" or another one.
+   "com.apple.inputmethod.SCIM.Shuangpin")
+  ;; enable the /cursor color/ mode
+  (sis-global-cursor-color-mode t)
+  ;; enable the /respect/ mode
+  (sis-global-respect-mode t)
+  ;; enable the /context/ mode for all buffers
+  (sis-global-context-mode t)
+  ;; enable the /inline english/ mode for all buffers
+  (sis-global-inline-mode t)
+  )
